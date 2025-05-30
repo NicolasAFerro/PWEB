75bd4788ca58172d8f0595e820f6bcc7d08e2d81
NicolasAFerro <nicolasalexandrinoferro@gmail.com>
2025-05-30 15:40:31 -0300

    iniciando tailwind

--- projNicolasFerro/src/App.jsx
+++ projNicolasFerro/src/App.jsx
@@ -5,7 +5,7 @@ import { Outlet } from 'react-router-dom';
 import NavBar from './components/NavBar';
 function App() {
   return (
-    <div className='App'>
+    <div className='bg-slate-300'>
       <NavBar></NavBar>
       <Outlet />
     </div>
--- projNicolasFerro/src/components/NavBar.jsx
+++ projNicolasFerro/src/components/NavBar.jsx
@@ -3,9 +3,18 @@ import { Link, NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div>
-      <NavLink to='/'>Lista de Livros</NavLink>
-      <NavLink to='/mylist'>Minha Lista</NavLink>
+    <div className=' bg-white p-8 shadow-2xl'>
+      <div className=' grid grid-cols-3'>
+        <h1 className='text-md font-bold '>Trabalho Final Pweb</h1>
+        <div className='border-2 flex gap-x-8 col-span-2'>
+          <NavLink to='/' className='hover:font-bold duration-200'>
+            Lista de Livros
+          </NavLink>
+          <NavLink to='/mylist' className='hover:font-bold  duration-200'>
+            Minha Lista
+          </NavLink>
+        </div>
+      </div>
     </div>
   );
 };
