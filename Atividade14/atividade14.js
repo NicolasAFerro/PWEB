ed16bf9291ae5e753335886f22ffc235822d5376
NicolasAFerro <nicolasalexandrinoferro@gmail.com>
2025-06-19 17:03:29 -0300

    limpando a branch

--- Atividade14/atividade14.js
+++
@@ -1,6 +0,0 @@
-let textInput = document.getElementById("fatec");
-function trocaLetras() {
-  if (document.getElementById("minusculo").checked)
-    textInput.value = textInput.value.toLowerCase();
-  else textInput.value = textInput.value.toUpperCase();
-}
